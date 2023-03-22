059ea06558ce1709145f1d0b8d982f5956a353b5
wad-junghasung <hasung.jung@catchtable.co.kr>
2023-03-22 14:04:45 +0900

    Initial Commit

---
+++ StoreSearchApp/StoreSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreSearchAppApp.swift
+//  StoreSearchApp
+//
+//  Created by hasung jung on 2023/03/22.
+//
+
+import SwiftUI
+
+@main
+struct StoreSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
